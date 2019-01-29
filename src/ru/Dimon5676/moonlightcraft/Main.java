174e7450437e21d05badb96b67ab18652b47d099
Dimon5676 <Dimon5676@yandex.ru>
2019-01-30 00:22:12 +0300

    Init commit

---
+++ src/ru/Dimon5676/moonlightcraft/Main.java
@@ -0,0 +1,16 @@
+package ru.Dimon5676.moonlightcraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
